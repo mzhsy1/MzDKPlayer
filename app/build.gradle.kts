913b094d357d6b937d7d3ec6f19e93cdd9c531e2
mzhsy1 <foreverwpwp@gmail.com>
2026-09-13 15:21:10 +0800

    加入图片基础操作功能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.mz.mzdkplayer"
         minSdk = 23
         targetSdk = 37
-        versionCode = 109
-        versionName = "1.17.2"
+        versionCode = 111
+        versionName = "1.17.3"
         ndk {
             //noinspection ChromeOsAbiSupport
             abiFilters += listOf("armeabi-v7a", "arm64-v8a","x86")
